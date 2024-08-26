f322c6429e4f2b702cfbd4dcc0242ca994bba42a
huancoi2032002 <huancoi2032002@gmail.com>
2024-08-27 01:31:28 +0700

    update FilterPackCol

--- src/components/Filter/FiltersPacks/FilterPackCol/FilterPackCol.tsx
+++ src/components/Filter/FiltersPacks/FilterPackCol/FilterPackCol.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode, useState, useEffect } from "react";
-import './Styles.scss'
+import './Styles.scss';
 import { BellIcon, Ellipse2Icon, EventIcon, GroupIcon, LightBuldIcon, NewsIcon } from "../../../../assets/iconpack";
 
 type FilterPackProps = {
@@ -9,35 +9,65 @@ type FilterPackProps = {
     onClick?: () => void;
 };
 
-const FilterPack: React.FC<FilterPackProps> = ({ label, icon, selected: defaultSelected = false, onClick }) => {
-    const [selected, setSelected] = useState(defaultSelected);
+const sidebarItems = [
+    {
+        title: 'Giới thiệu',
+        icon: <LightBuldIcon />,
+    },
+    {
+        title: 'Tin tức',
+        icon: <NewsIcon />,
+    },
+    {
+        title: 'Sự kiện',
+        icon: <EventIcon />,
+    },
+    {
+        title: 'Thông báo',
+        icon: <BellIcon />,
+    },
+    {
+        title: 'Tin cổ đông',
+        icon: <NewsIcon />,
+    },
+    {
+        title: 'Hoạt động đoàn thể',
+        icon: <GroupIcon />,
+    },
+];
 
-    useEffect(() => {
-        setSelected(defaultSelected);
-    }, [defaultSelected]);
-
-    const handleClick = () => {
-        setSelected(!selected);
-        if (onClick) onClick();
-    };
-
+const FilterPack: React.FC<FilterPackProps> = ({ label, icon, selected = false, onClick }) => {
     return (
         <div
-            className={`flex w-[322px] p-5 items-center gap-3 rounded-lg cursor-pointer filterpack-custom ${selected ? 'bg-[#0054A6]' : 'bg-[#FFF]'}`}
-            onClick={handleClick}
+            className={`flex w-[322px] p-5 items-center gap-3 rounded-lg cursor-pointer filterpack-custom 
+                ${selected ? 'bg-[#0054A6] text-white hover:text-white' : 'bg-[#FFF] text-[#6698CA] hover:text-blue-500'}`
+            }
+            onClick={onClick}
         >
             <div className="flex size-6 justify-center items-center flex-shrink-0">
-                <div className={`icon ${selected ? 'fill-[#FFF] stroke-[#FFF]' : 'fill-[#0054A6] stroke-[#0054A6]'}`}>
+                <div className={`icon ${selected ? 'fill-[#FFF] stroke-[#FFF]' : 'fill-blue-500 stroke-[#0054A6]'}`}>
                     {icon}
                 </div>
             </div>
-            <p className={`text-xl font-semibold ${selected ? 'text-[#FFF]' : 'text-[#6698CA]'}`}>{label}</p>
+            <p className={`text-xl font-semibold`}>
+                {label}
+            </p>
         </div>
     );
 };
 
 const FilterPackCol = () => {
-    const [selectedLabel, setSelectedLabel] = useState("Giới thiệu");
+    const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
+
+    const handleItemClick = (label: string) => {
+        setSelectedLabels(prevSelected => {
+            if (prevSelected.includes(label)) {
+                return prevSelected.filter(item => item !== label);
+            } else {
+                return [...prevSelected, label];
+            }
+        });
+    };
 
     return (
         <div className="w-[362px] h-[1028px] custom-bg-filterpackcol flex-shrink-0 p-5">
@@ -45,46 +75,19 @@ const FilterPackCol = () => {
                 <span className="custom-title-packs font-philosopher">Chủ đề bài viết</span>
             </div>
             <div className="inline-flex flex-col items-start gap-5">
-                <FilterPack
-                    label="Giới thiệu"
-                    icon={<LightBuldIcon />}
-                    selected={selectedLabel === "Giới thiệu"}
-                    onClick={() => setSelectedLabel("Giới thiệu")}
-                />
-                <FilterPack
-                    label="Tin tức"
-                    icon={<NewsIcon />}
-                    selected={selectedLabel === "Tin tức"}
-                    onClick={() => setSelectedLabel("Tin tức")}
-                />
-                <FilterPack
-                    label="Sự kiện"
-                    icon={<EventIcon />}
-                    selected={selectedLabel === "Sự kiện"}
-                    onClick={() => setSelectedLabel("Sự kiện")}
-                />
-                <FilterPack
-                    label="Thông báo"
-                    icon={<BellIcon />}
-                    selected={selectedLabel === "Thông báo"}
-                    onClick={() => setSelectedLabel("Thông báo")}
-                />
-                <FilterPack
-                    label="Tin cổ đông"
-                    icon={<NewsIcon />}
-                    selected={selectedLabel === "Tin cổ đông"}
-                    onClick={() => setSelectedLabel("Tin cổ đông")}
-                />
-                <FilterPack
-                    label="Hoạt động đoàn thể"
-                    icon={<GroupIcon />}
-                    selected={selectedLabel === "Hoạt động đoàn thể"}
-                    onClick={() => setSelectedLabel("Hoạt động đoàn thể")}
-                />
+                {sidebarItems.map(item => (
+                    <FilterPack
+                        key={item.title}
+                        label={item.title}
+                        icon={item.icon}
+                        selected={selectedLabels.includes(item.title)}
+                        onClick={() => handleItemClick(item.title)}
+                    />
+                ))}
             </div>
             <Ellipse2Icon />
         </div>
-    )
-}
+    );
+};
 
 export default FilterPackCol;
--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,27 @@ export default {
         'custom-light-blue': '#9FF',
         'blue-primary-600': '#003F7D',
         'customColor': '#99FFFF',
+        cyan: '#99FFFF',
+        white: '#FFF',
+        black: '#000',
+
+        //background
+        'blue-background': '#002F5CCC',
+        'secondary-background': '#4E95DBB2',
+        'footer-background': '#002549CC',
+
+        //blue
+        'blue-50': '#E6EEF7',
+        'blue-300': '#6698CA',
+        'blue-500': '#0054A6',
+        'blue-600': '#003F7D',
+
+        //gray
+        'gray-50': '#F0F0F0',
+        'gray-400': '#858585',
+        'gray-500': '#666666',
+        'gray-600': '#4D4D4D',
+        'gray-700': '#333333',
       },
       height: {
         'h-nav': '120px',
