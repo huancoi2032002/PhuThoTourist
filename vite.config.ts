734da6a1cb2796571b5f6b37cfe5b1878ebe73d7
huancoi2032002 <huancoi2032002@gmail.com>
2024-09-07 03:27:05 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/huancoi2032002/PhuThoTourist.git',
+  base: '/PhuThoTourist/', // Đặt đường dẫn cơ sở cho GitHub Pages
   plugins: [react()],
 })
