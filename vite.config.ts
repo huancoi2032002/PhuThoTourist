47ba875513902168fbe350d48b6d76e9a579f3cd
huancoi2032002 <huancoi2032002@gmail.com>
2024-09-07 03:23:24 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://github.com/huancoi2032002/PhuThoTourist.git',
   plugins: [react()],
 })
